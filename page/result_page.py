4046fa23b3c917b2bca09db8824668ffe4d37958
aleks <dert6705@gmail.com>
2019-03-25 19:09:48 +0200

    end w

--- page/result_page.py
+++ page/result_page.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from page.base_page import BasePage
 
@@ -9,6 +11,9 @@ class ResultPage(BasePage):
     LIST_RESULT = (By.CSS_SELECTOR, "div.rc")
     TEXT_RESULT = (By.CSS_SELECTOR, "h3")
     LINK_RESULT = (By.CSS_SELECTOR, "a")
+    _TOOLS = (By.XPATH, "//div[@role='navigation']/div/div/div/div[2]/a")
+    _TIME_SEARCH = (By.XPATH, "//div[@role='navigation']/div[2]/div/div[3]")
+    _TIME_ONE_HOUR = (By.XPATH, "//div[@role='navigation']/div[2]/div//ul[2]//li[2]/a")
 
     def find_text_result(self, text):
         result = []
@@ -18,3 +23,19 @@ class ResultPage(BasePage):
                 result[i].find_element(*self.LINK_RESULT).send_keys(Keys.LEFT_CONTROL, Keys.SHIFT, Keys.ENTER)
         """переключаемся на другую вкладку"""
 
+    def tools_click(self):
+        self.driver.find_element(*self._TOOLS).click()
+        return self
+
+    def time_click(self):
+        element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(*self._TIME_SEARCH))
+        element.click()
+        return self
+
+    def time_click_one_hour(self):
+        element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(*self._TIME_ONE_HOUR))
+        element.click()
+        return self
+
+    def time_get_text(self):
+        return self.driver.find_element(*self._TIME_ONE_HOUR).text
--- test/ui/test_google.py
+++ test/ui/test_google.py
@@ -16,3 +16,13 @@ class TestGoogle2(TestBase):
         self.driver.switch_to.window(self.driver.window_handles[1])
         text = GbsfoPage(self.driver).get_mail()
         assert (text == "sales@gbsfo.com")
+
+    def test_tools_time(self):
+        StartPage(self.driver).\
+            open_page("https://www.google.com").\
+            send_request("google")
+        ResultPage(self.driver).\
+            tools_click().\
+            time_click().\
+            time_click_one_hour()
+        assert (ResultPage(self.driver).time_get_text() == 'За час')
